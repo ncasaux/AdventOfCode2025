4fe9d2c43c2985e148104455a2541a9231bc4b46
ncasaux <nicolas.casaux@outlook.com>
2025-12-13 20:43:57 +0100

    day4

---
+++ day4b/main.py
@@ -0,0 +1,36 @@
+import aocd
+
+f = aocd.get_data(day=4, year=2025)
+
+maze = []
+length = len(f.splitlines()[0])
+maze.append(list("#" * (length + 2)))
+for line in f.splitlines():
+    maze.append(list(f"#{line}#"))
+maze.append(list("#" * (length + 2)))
+
+rolls = 0
+removed_rolls = []
+
+while len(removed_rolls) != 0 or rolls == 0:
+    removed_rolls = []
+    for y in range(1, len(maze) - 1):
+        for x in range(1, len(maze[y]) - 1):
+            if maze[y][x] == "@":
+                neighbours = 0
+                if maze[y - 1][x - 1] == "@": neighbours += 1
+                if maze[y - 1][x] == "@": neighbours += 1
+                if maze[y - 1][x + 1] == "@": neighbours += 1
+                if maze[y][x - 1] == "@": neighbours += 1
+                if maze[y][x + 1] == "@": neighbours += 1
+                if maze[y + 1][x - 1] == "@": neighbours += 1
+                if maze[y + 1][x] == "@": neighbours += 1
+                if maze[y + 1][x + 1] == "@": neighbours += 1
+                if neighbours < 4:
+                    rolls += 1
+                    removed_rolls.append((y, x))
+
+    for i in removed_rolls:
+        maze[i[0]][i[1]] = "."
+
+print(f"rolls: {rolls}")
